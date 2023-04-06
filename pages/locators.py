57cb197a4699251bcc0e981d0dbaeed23b3db679
asokolov19996@gmail.com <asokolov19996@gmail.com>
2023-04-06 08:06:06 +0600

    add class LoginPageLocators()

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_URL = "http://selenium1py.pythonanywhere.com/ru/accounts/login/1"
+    LOGIN_FORM = (By.ID, "login_form1")
+    REGISTER_FORM = (By.ID, "register_form1")
