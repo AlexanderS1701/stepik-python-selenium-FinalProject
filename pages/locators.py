a36e91fa34b4ddff71c9bf056167ee5643c0c144
asokolov19996@gmail.com <asokolov19996@gmail.com>
2023-04-10 09:28:39 +0600

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BTN = (By.CLASS_NAME, 'btn-add-to-basket')
+    PRODUCT_ADDED_MESSAGE = (By.CSS_SELECTOR, ".alert-success strong")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main h1")
+    SUCCESS_MESSAGES = (By.ID, "messages")
