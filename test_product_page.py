f4affc5627674a622a8010c2842febd4807c6286
asokolov19996@gmail.com <asokolov19996@gmail.com>
2023-04-11 08:16:35 +0600

    added links with promo for test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -17,7 +17,6 @@ import pytest
              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
     )
 def test_guest_can_add_product_to_basket(browser, link):
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
